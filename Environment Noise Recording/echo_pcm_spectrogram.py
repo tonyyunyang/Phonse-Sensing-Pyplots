e443eed283649f18c4afb16c6b15f1e9052e61fd
tonyyunyang <tonyyunyang@outlook.com>
2023-05-12 13:28:14 +0200

    Latest.

--- Environment Noise Recording/echo_pcm_spectrogram.py
+++ Environment Noise Recording/echo_pcm_spectrogram.py
@@ -3,7 +3,7 @@
 # from scipy import signal
 
 # # Load the audio data from the .pcm file
-# data = np.fromfile('Chirp_Echo_Testing/Final/20230510_172336.pcm', dtype=np.int16)
+# data = np.fromfile('On-site Test 1/C1/20230511_172509_testing.pcm', dtype=np.int16)
 
 # # Define the sampling rate (in Hz)
 # fs = 44100
@@ -16,7 +16,8 @@
 # # spectrogram = spectrogram[:len(frequencies), :]
 
 # # Plot the spectrogram
-# plt.pcolormesh(times, frequencies, 10 * np.log10(spectrogram), cmap='plasma')
+# # plt.pcolormesh(times, frequencies, 10 * np.log10(spectrogram), cmap='plasma')
+# plt.pcolormesh(times, frequencies, 10 * np.log10(spectrogram), cmap='jet')
 # plt.ylabel('Frequency [Hz]')
 # plt.xlabel('Time [sec]')
 
@@ -33,29 +34,47 @@
 
 
 import numpy as np
-from scipy.io import wavfile
 import matplotlib.pyplot as plt
 
-# set the path to the recording file
-filename = "On-site Test 1/20230511_182815_testing.pcm"
+# Read the .pcm file
+with open('On-site Test 1/C1/20230511_172509.pcm', 'rb') as f:
+    pcm_data = f.read()
 
-# define the sampling rate and window size for the spectrogram
-sr = 44100
-win_size = int(0.01 * sr)  # 10ms window
+# Convert the raw PCM data to a NumPy array
+data = np.frombuffer(pcm_data, dtype='int16')
 
-# read the binary data from the PCM file and convert to integer samples
-with open(filename, 'rb') as f:
-    pcm_data = np.frombuffer(f.read(), np.int16)
+# Set up audio recording parameters
+sample_rate = 44100
 
-# compute the spectrogram using a Hanning window and 50% overlap
-spectrum, freqs, t, im = plt.specgram(
-    pcm_data, NFFT=win_size, Fs=sr, window=np.hanning(win_size),
-    noverlap=win_size // 2, cmap=plt.cm.jet)
-
-# set the labels for the plot
+# Plot the spectrogram
+plt.specgram(data, Fs=sample_rate, cmap='jet', vmin=-60, vmax=20)
+plt.xlabel('Time [sec]')
+plt.ylim([15000, 20000])
+plt.ylabel('Frequency [Hz]')
+plt.title('Spectrogram of PCM file')
 plt.colorbar()
-plt.xlabel('Time (s)')
-plt.ylabel('Frequency (Hz)')
-
-# display the spectrogram
 plt.show()
+
+
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# # Read the .pcm file
+# with open('On-site Test 1/C1/20230511_172509_testing.pcm', 'rb') as f:
+#     pcm_data = f.read()
+
+# # Convert the raw PCM data to a NumPy array
+# data = np.frombuffer(pcm_data, dtype='int16')
+
+# # Set up audio recording parameters
+# sample_rate = 44100
+
+# # Plot the spectrogram
+# plt.specgram(data, Fs=sample_rate, cmap='jet', vmin=-60, vmax=20)
+# plt.xlabel('Time [sec]')
+# plt.ylabel('Frequency [Hz]')
+# plt.ylim([15000, 20000])
+# plt.title('Spectrogram of PCM file')
+# plt.colorbar()
+# plt.show()
